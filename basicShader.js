8de5e3dfa29a8d56d7ca29f2b8bbaeb6db05cb28
Unknown <meline.bour-lang@etu.unistra.fr>
2018-12-04 21:25:29 +0100

    sphere

--- basicShader.js
+++ basicShader.js
@@ -6,14 +6,37 @@
 
 var basicVertexShader = `#version 300 es
 
-uniform mat4 orthoMatrix;
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;
+uniform mat3 normalMatrix;
 
-in vec2 position_in;
+in vec3 position_in;
+in vec3 normal_in;
+
+out vec3 Col;
+
+// emplacement de la lumière : en haut de la caméra (0, 0, 0)
+const vec3 pos_lum = vec3(0.0, 0.5, 0.0);
+const float alpha = 100.0;
 
 void main()
 {
-	gl_PointSize = 8.0;
-	gl_Position = orthoMatrix*vec4(position_in, 0.0, 1.0);
+	vec4 P4 = viewMatrix * vec4(position_in,1);
+
+	gl_Position = projectionMatrix * P4;
+
+	vec3 P = P4.xyz;
+	vec3 No = normalMatrix * normal_in;
+	vec3 N = normalize(No);
+	vec3 L = normalize(pos_lum-P);
+	float lamb = 0.15+0.85*clamp(dot(N, L), 0.0, 1.0); //entre 0 et 1
+
+	vec3 V = normalize(-P);
+	vec3 R = reflect(-L, N);
+
+	float spec = pow(clamp(dot(V,R), 0.0, 1.0), alpha);
+
+	Col = lamb*vec3(1,0,0) + spec*vec3(1, 1, 1); //ou normal_in
 }
 
 `;
@@ -21,16 +44,15 @@ void main()
 /** BASIC FRAGMENT SHADER **/
 
 var basicFragmentShader = `#version 300 es
-
 precision highp float;
 
-uniform vec3 color;
+in vec3 Col;
 
 out vec4 frag_out;
 
 void main()
 {
-	frag_out = vec4(color, 1.0);
+	frag_out = vec4(abs(Col), 1);
 }
 
 `;
--- main.js
+++ main.js
@@ -4,15 +4,18 @@
 var basicProgram = null;
 
 // vao
-var vao1 = null;
-var ebo1 = null;
-var b=0;
-var animation = null;
-var mode;
+// var vao1 = null;
+// var ebo1 = null;
+// var b=0;
+// var animation = null;
+// var mode;
+
+// mesh renderers
+let sunRenderer = null;
 
 function init_wgl()
 {
-  drawUserInterface();
+  // drawUserInterface();
 
 	/** PROGRAM SHADER**/
 	basicProgram = ShaderProgram(basicVertexShader, basicFragmentShader, 'basicProgram');
@@ -20,6 +23,23 @@ function init_wgl()
 	/** VBO **/
 
 	/** VAO **/
+
+
+  let sun = Mesh.Sphere(200);
+  /*use : position, normals ; doesn't use : texture*/
+  sunRenderer = sun.renderer(true, true, false);
+  scene_camera.show_scene(sun.BB);
+
+
+  FileDroppedOnCanevas( (blob) =>
+	{
+		Mesh.load(blob).then((mesh) =>
+		{
+			mesh_rend = mesh.renderer(true,true,false);
+			scene_camera.show_scene(mesh.BB);
+			update_wgl();
+		});
+	});
 
 }
 
@@ -29,11 +49,16 @@ function draw_wgl()
 	gl.enable(gl.DEPTH_TEST);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+  // les matrices sont deduites de la camera
+  const projection_matrix = scene_camera.get_projection_matrix();
+	const view_matrix = scene_camera.get_view_matrix();
 
 	basicProgram.bind();
-	update_uniform('orthoMatrix', ortho2D);
+	update_uniform('viewMatrix', view_matrix);
+	update_uniform('normalMatrix', view_matrix.inverse3transpose());
+	update_uniform('projectionMatrix', projection_matrix);
+	sunRenderer.draw(gl.TRIANGLES);
 	unbind_shader();
-  
 }
 
-launch_2d();
+launch_3d();
